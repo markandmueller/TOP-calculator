6743fcb9c13234e5d7729c29d95e9893574a213e
markandmueller <markamueller@mac.com>
2022-12-17 09:25:26 -0800

    current snapshot, errors need fixing
    Able to operate on 1 number and then another and equals. But if you try to operate more than one time, the results aren't correct.
    
    Going to take a few steps backward and see how I can improve the logic so the math is correct no matter how many operators you use.

--- app.js
+++ app.js
@@ -1,74 +1,136 @@
-let numDisplay = 0;
-let operator = null;
-let a = null;
-let b = null;
-
+let operator = undefined;
+let a = undefined;
+let b = undefined;
+let result = undefined;
+let numsArray = [];
+let numDisplay = 0; // text content displayed
+const display = document.querySelector("#display");
 const del = document.querySelector("#delete");
 const clear = document.querySelector("#clear");
-const display = document.querySelector("#display");
+clear.addEventListener('click', clearCalc);
 const buttonsMain = document.querySelector("#buttonsMain");
-
-const operatorBtn = buttonsMain.querySelectorAll(".operatorBtn");
-operatorBtn.forEach((operatorBtn) => {
-    operatorBtn.addEventListener('click', function () {
-        // console.log('ya boi');
-    });
-});
-
 const numBtn = buttonsMain.querySelectorAll(".numBtn");
 numBtn.forEach((numBtn) => {
     numBtn.addEventListener('click', populateDisplay);
 });
 
 function populateDisplay() {
-    numDisplay = this.value;
-    display.textContent = this.value;
+    if (a === undefined) {
+        // then push the pressed number keys into the A Array
+        numsArray.push(this.value);
+        numDisplay = numsArray.join("");
+        display.textContent = numDisplay;
+        console.log(getFuncName());
+    }
+    else if (b === undefined) {
+        numsArray.push(this.value);
+        numDisplay = numsArray.join("");
+        display.textContent = numDisplay;
+        console.log(getFuncName());
+    }
 }
 
-function add(a, b) {
-    let addResult = a + b;
-    return addResult;
-}
-
-function subtract(a, b) {
-    let subResult = a - b;
-    return subResult;
-}
-
-function multiply(a, b) {
-    let multResult = a * b;
-    return multResult;
-}
-
-function divide(a, b) {
-    let divResult = a / b;
-    return divResult;
-}
+const operatorBtn = buttonsMain.querySelectorAll(".operatorBtn");
+operatorBtn.forEach((operatorBtn) => {
+    operatorBtn.addEventListener('click', function () {
+        if (a !== undefined) {
+            clickEquals();
+            console.log(getFuncName());
+        }
+        else {
+            operator = this.value;
+            a = parseInt(numDisplay);
+            numsArray = [];
+            console.log(getFuncName());
+        };
+    });
+});
 
 function operate(operator, a, b) {
-    if (operator === add) {
-        console.log(add(a, b));
+    if (operator === 'add') {
+        add(a, b);
+        console.log(getFuncName());
     }
-    else if (operator === subtract) {
-        console.log(subtract(a, b));
+    else if (operator === 'subtract') {
+        subtract(a, b);
+        console.log(getFuncName());
     }
-    else if (operator === multiply) {
-        console.log(multiply(a, b));
+    else if (operator === 'multiply') {
+        multiply(a, b);
+        console.log(getFuncName());
     }
-    else if (operator === divide) {
-        console.log(divide(a, b));
+    else if (operator === 'divide') {
+        divide(a, b);
+        console.log(getFuncName());
     }
     else {
         console.log('whoops - i\m getting curious');
+        console.log(getFuncName());
     }
 }
 
-// operate(add, 10, 10);
-// operate(subtract, 10, 10);
-// operate(multiply, 10, 10);
-// operate(divide, 10, 10);
+function add(a, b) {
+    result = a + b;
+    console.log(result);
+    console.log(getFuncName());
+    return result;
+}
+function subtract(a, b) {
+    result = a - b;
+    console.log(result);
+    console.log(getFuncName());
+    return result;
+}
+function multiply(a, b) {
+    result = a * b;
+    console.log(result);
+    console.log(getFuncName());
+    return result;
+}
+function divide(a, b) {
+    result = a / b;
+    console.log(result);
+    console.log(getFuncName());
+    return result;
+}
+
+const equalsBtn = document.querySelector("#equalsBtn");
+equalsBtn.addEventListener('click', clickEquals);
+
+function clickEquals() {
+    b = parseInt(numDisplay);
+    operate(operator, a, b);
+    a = result;
+    display.textContent = a;
+    b = undefined;
+    console.log(getFuncName());
+}
+
+function clearCalc() {
+    operator = undefined;
+    a = undefined;
+    b = undefined;
+    result = undefined;
+    numsArray = [];
+    numDisplay = 0;
+    display.textContent = 0;
+    console.log(getFuncName());
+}
+
+function getFuncName() {
+    console.log(`================ ${getFuncName.caller.name} ==================`);
+    logVars();
+}
+
+function logVars() {
+    console.log(`=== CURRENT VALUES:`);
+    console.log(`Operator: ${operator}`);
+    console.log(`A: ${a}, B: ${b}`);
+    console.log(`NumsArray: ${numsArray}`);
+    console.log(`NumDisplay: ${numDisplay}`);
+}
 
 function test() {
     console.log('test worked');
 }
-// test();
+
